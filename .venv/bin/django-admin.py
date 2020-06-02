8e3fd3797d88803a0bbb518e080d4051563b7d67
Mike A <jimalik2525@gmail.com>
2020-06-02 16:48:32 -0400

    Had to reinstall the venv since it was breaking my fork, please... please don't ever commit the .venv folder again...

--- .venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/D.Middleton/Kenzie_Assignments/Kenzie_Q4/recipe_box_v1/.venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
