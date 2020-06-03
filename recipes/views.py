7c9596be4afaa9a47276a816012e1bd25639bcaa
Mike A <jimalik2525@gmail.com>
2020-06-03 10:23:59 -0400

    Fixed last minute issue

--- recipes/views.py
+++ recipes/views.py
@@ -108,8 +108,8 @@ def recipe(request, recipe_id):
 
 
 def author(request, author_id):
-    the_author = Author.objects.get(id=author_id)
-    favorite_recipes = the_author.user.favorite.all()
+    author = Author.objects.get(id=author_id)
+    favorite_recipes = author.user.favorite.all()
     data = Author.objects.filter(id=author_id)
     recipes_published = Recipe.objects.all()
     return render(request, 'author.html', {'data': data, 'recipes_published': recipes_published,
